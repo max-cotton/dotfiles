7b292926c4605e8201171acbb85c61b7e47e4953
Max Cotton <maxcotton22@gmail.com>
2024-12-14 23:20:18 +0000

    chore: Add kitty-cava-session config file

--- .config/old-autostart-scripts/kitty-autostart.sh
+++ .config/old-autostart-scripts/kitty-autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kitty --start-as fullscreen --detach cava
-kitty
+kitty --start-as fullscreen --session ~/.config/kitty/kitty-cava-session.conf --detach
+kitty --session ~/.config/kitty/kitty-session.conf --detach
 
