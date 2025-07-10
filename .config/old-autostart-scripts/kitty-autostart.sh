44ecc4688c0b6d1e87e9fff23bfb6a887111f512
Max Cotton <maxcotton22@gmail.com>
2025-07-10 02:11:34 +0100

    chore: Update kitty autostart script

--- .config/old-autostart-scripts/kitty-autostart.sh
+++ .config/old-autostart-scripts/kitty-autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kitty --start-as fullscreen --session ~/.config/kitty/kitty-cava-session.conf --detach
 kitty --session ~/.config/kitty/kitty-session.conf --detach
+kitty --session ~/.config/kitty/kitty-cmus-session.conf --detach
 
