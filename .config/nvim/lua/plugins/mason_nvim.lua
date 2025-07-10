7bc68da11f0285745675d1a9848e1e77186c9de9
Max Cotton <maxcotton22@gmail.com>
2025-07-11 00:42:27 +0100

    feat: Neovim 2.0

---
+++ .config/nvim/lua/plugins/mason_nvim.lua
@@ -0,0 +1,3 @@
+return {
+    { 'mason-org/mason.nvim', config = true, opts = {} },
+}
---
+++ .config/nvim/lua/plugins/nvim_surround.lua
@@ -0,0 +1,6 @@
+return {
+    { 'kylechui/nvim-surround', event = "VeryLazy", config = function()
+	  	require("nvim-surround").setup({})
+	  end
+	},
+}
