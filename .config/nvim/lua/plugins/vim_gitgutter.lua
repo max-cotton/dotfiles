f569eceea443984abf2a684d2204999acee6e1af
Max Cotton <maxcotton22@gmail.com>
2025-07-10 02:12:44 +0100

    chore: Reformat neovim plugin code

--- .config/nvim/lua/plugins/vim_gitgutter.lua
+++ .config/nvim/lua/plugins/vim_gitgutter.lua
@@ -1,7 +1,7 @@
 return {
     { 'airblade/vim-gitgutter',
       config = function()
-      	vim.cmd([[highlight GitGutterAdd guifg=#009900 ctermfg=2]])
+          vim.cmd([[highlight GitGutterAdd guifg=#009900 ctermfg=2]])
 	      vim.cmd([[highlight GitGutterChange guifg=#bbbb00 ctermfg=3]])
 	      vim.cmd([[highlight GitGutterDelete guifg=#ff2222 ctermfg=1]])
 	      vim.cmd([[highlight clear SignColumn]])
